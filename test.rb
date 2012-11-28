fa4aefb47c15e1c05022c33a876265b6efaf8852
Alden Turner <alonzopt@gmail.com>
2012-11-28 07:19:51 -0600

    updated test.rb for modified api endpoints

--- test.rb
+++ test.rb
@@ -11,31 +11,31 @@ class ResumeAPITest < Test::Unit::TestCase
   end
   
   def test_resume_all
-    get '/resume'
+    get '/api/resume'
     assert last_response.ok?
     assert_equal $RESUME, JSON.parse(last_response.body)
   end
   
   def test_resume_contact
-    get '/resume/contact'
+    get '/api/resume/contact'
     assert last_response.ok?
     assert_equal $RESUME["contact"], JSON.parse(last_response.body)
   end
   
   def test_resume_education
-    get '/resume/education'
+    get '/api/resume/education'
     assert last_response.ok?
     assert_equal $RESUME["education"], JSON.parse(last_response.body)
   end
   
   def test_resume_jobs
-    get '/resume/jobs'
+    get '/api/resume/jobs'
     assert last_response.ok?
     assert_equal $RESUME["jobs"], JSON.parse(last_response.body)
   end
   
   def test_resume_skills
-    get '/resume/skills'
+    get '/api/resume/skills'
     assert last_response.ok?
     assert_equal $RESUME["skills"], JSON.parse(last_response.body)
   end
